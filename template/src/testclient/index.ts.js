574e96e0356f605572fc5077309d5aa4314c9693
Jonas Lagoni <jonas-lt@live.dk>
2021-03-30 15:06:15 +0200

    Refactored test client

--- template/src/testclient/index.ts.js
+++ template/src/testclient/index.ts.js
@@ -7,7 +7,7 @@ import { Reply } from '../../../components/index/reply';
 import { Request } from '../../../components/index/request';
 import { isRequestReply, isReplier, isRequester, isPubsub} from '../../../utils/index';
 // eslint-disable-next-line no-unused-vars
-import { AsyncAPIDocument, ChannelParameter } from '@asyncapi/parser';
+import { IntentAsyncAPIDocument, ChannelParameter } from '@asyncapi/parser';
 
 /**
  * @typedef TemplateParameters
@@ -19,29 +19,30 @@ import { AsyncAPIDocument, ChannelParameter } from '@asyncapi/parser';
 /**
  * @typedef RenderArgument
  * @type {object}
- * @property {AsyncAPIDocument} asyncapi received from the generator.
+ * @property {IntentAsyncAPIDocument} asyncapi received from the generator.
  * @property {TemplateParameters} params received from the generator.
  */
 
 /**
  * Return the correct channel functions for the test client on whether a channel is `pubSub` or `requestReply`
  * 
- * @param {AsyncAPIDocument} asyncapi 
+ * @param {IntentAsyncAPIDocument} asyncapi 
  * @param {TemplateParameters} params 
  */
 // eslint-disable-next-line sonarjs/cognitive-complexity
 function getChannelWrappers(asyncapi, params) {
-  let channelWrappers = asyncapi.channels();
-  channelWrappers = Object.keys(channelWrappers).length ? Object.entries(channelWrappers).map(([channelName, channel]) => {
-    const publishMessage = channel.publish() ? channel.publish().message(0) : undefined;
-    const subscribeMessage = channel.subscribe() ? channel.subscribe().message(0) : undefined;
+  let channelWrappers = [];
+  channelWrappers = asyncapi.channels().map((channel) => {
+    const publishMessage = channel.messagesPublishes();
+    const subscribeMessage = channel.messagesSubscribes();
     const defaultContentType = asyncapi.defaultContentType();
     const channelDescription = channel.description();
     const channelParameters = channel.parameters();
+    const channelPath = channel.path();
     if (isRequestReply(channel)) {
       if (isRequester(channel)) {
         return Reply(defaultContentType, 
-          channelName, 
+          channelPath, 
           publishMessage,
           subscribeMessage,
           channelDescription,
@@ -52,7 +53,7 @@ function getChannelWrappers(asyncapi, params) {
       if (isReplier(channel)) {
         return Request(
           defaultContentType, 
-          channelName, 
+          channelPath, 
           publishMessage,
           subscribeMessage,
           channelDescription,
@@ -65,7 +66,7 @@ function getChannelWrappers(asyncapi, params) {
       if (channel.hasSubscribe()) {
         return Subscribe(
           defaultContentType, 
-          channelName, 
+          channelPath, 
           subscribeMessage, 
           channelDescription, 
           channelParameters);
@@ -73,13 +74,13 @@ function getChannelWrappers(asyncapi, params) {
       if (channel.hasPublish()) {
         return Publish(
           defaultContentType, 
-          channelName, 
+          channelPath, 
           publishMessage, 
           channelDescription, 
           channelParameters);
       }
     }
-  }) : '';
+  });
   return channelWrappers;
 }
 
